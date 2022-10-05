3d23b3d6301f4bf3a154f8728250c13f559486c9
mhdarshad2000 <mhdarshad2937@gmail.com>
2022-10-05 20:24:00 +0530

    post reaction start

--- backend/controllers/upload.js
+++ backend/controllers/upload.js
@@ -32,7 +32,7 @@ const uploadToCloudinary = async (file, path) => {
       (err, res) => {
         if (err) {
           removeTmp(file.tempFilePath);
-          return res.status(400).json({ message: "Upload image failed.",err:err });
+          return res.status(400).json({ message: "Upload image failed." });
         }
         resolve({
           url: res.secure_url,
--- frontend/src/component/posts/posts.js
+++ frontend/src/component/posts/posts.js
@@ -1,7 +1,8 @@
 import "./style.scss";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
-import Public from "../../svg/public"
+import Public from "../../svg/public";
+import Dots from "../../svg/dots";
 
 export default function Post({ post }) {
   return (
@@ -26,11 +27,65 @@ export default function Post({ post }) {
             </div>
           </div>
           <div className="post_profile_privacy_date">
-            <Moment fromNow interval={30} >{post.createdAt}</Moment>. <Public color="#828387" />
+            <Moment fromNow interval={30}>
+              {post.createdAt}
+            </Moment>
+            . <Public color="#828387" />
           </div>
         </Link>
+        <div className="post_header_right hover1">
+          <Dots color="#828387" />
+        </div>
       </div>
+      {post.background ? (
+        <div
+          className="post_bg"
+          style={{ backgroundImage: `url(${post.background})` }}
+        >
+          <div className="bg_text">{post.text}</div>
+        </div>
+      ) : (
+        <>
+          <div className="post_text">{post.text}</div>
+          {post.images && post.images.length && (
+            <div
+              className={
+                post.images.length === 1
+                  ? "grid_1"
+                  : post.images.length === 2
+                  ? "grid_2"
+                  : post.images.length === 3
+                  ? "grid_3"
+                  : post.images.length === 4
+                  ? "grid_4"
+                  : post.images.length >= 5 && "grid_5"
+              }
+            >
+              {post.images.slice(0, 5).map((img, i) => (
+                <img src={img.url} key={i} alt="" className={`img-${i}`} />
+              ))}
+              {post.images.length > 5 ? (
+                <div className="more_pics_shadow">
+                  +{post.images.length - 5}
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          )}
+        </>
+      )}
+      <div className="post_infos">
+        <div className="reacts_count">
+          <div className="reacts_count_imgs"></div>
+          <div className="reacts_count_num"></div>
+        </div>
+        <div className="to_right">
+          <div className="comments_count">13 comments</div>
+          <div className="share_count">1 share</div>
+        </div>
+      </div>
+      {/* video 5 (04.05 minutes) */}
     </div>
   );
 }
- 
